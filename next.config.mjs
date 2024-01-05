4a0b03e45fc03ffc5facdf0cb7da4bd53814a169
CarlosDavidAraujo <cdavidav19@gmail.com>
2024-01-05 14:51:47 -0300

    usando fontes locais

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  output: 'standalone',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.worker\.js$/,
