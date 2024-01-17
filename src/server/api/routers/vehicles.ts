b23bc82b24019e94f6096962d46a7d27e658219f
CarlosDavidAraujo <cdavidav19@gmail.com>
2024-01-17 15:08:50 -0300

    turno do veiculo agora se baseia na data de envio e não na data da ocorrencia

--- src/server/api/routers/vehicles.ts
+++ src/server/api/routers/vehicles.ts
@@ -4,7 +4,6 @@ import * as z from "zod";
 import { formatServerDateRange } from "@/utils/formatServerDateRange";
 import { getTurnFilterQuery } from "@/utils/getTurnQuery";
 import { addHours } from "date-fns";
-import { turnosVeiculos } from "@/constants/turnos";
 import { dateRangeSchema, turnoSchema } from "@/constants/zod-schemas";
 
 export const vehicleRouter = createTRPCRouter({
@@ -19,11 +18,8 @@ export const vehicleRouter = createTRPCRouter({
         turn: turnoSchema,
       }),
     )
-    .mutation(async ({ input }) => {
+    .query(async ({ input }) => {
       const { from, to } = formatServerDateRange(input.dateRange);
-      const turnoDeVeiculo = turnosVeiculos.some(
-        (turno) => turno.label === input.turn.label,
-      );
       return await db.ocorrencia.findMany({
         select: {
           OcorrenciaID: true,
@@ -35,15 +31,27 @@ export const vehicleRouter = createTRPCRouter({
               MotivoDS: true,
             },
           },
+          OcorrenciaMovimentacao: {
+            select: { EnvioEquipeDT: true, VeiculoID: true },
+            orderBy: { EnvioEquipeDT: "asc" },
+          },
         },
         where: {
-          DtHr: {
-            gte: turnoDeVeiculo ? addHours(from, 7) : addHours(from, 1), //primeiro turno inicia as 1h. 7h caso seja se veiculos
-            lt: turnoDeVeiculo ? addHours(to, 7) : addHours(to, 1), //ultimo turno encerra as 1h. 7h caso seja se veiculos
-          },
           OcorrenciaMovimentacao: {
             some: {
-              VeiculoID: input.vehicleId,
+              AND: {
+                VeiculoID: input.vehicleId,
+                EnvioEquipeDT: {
+                  gte:
+                    input.turn.category === "veiculo"
+                      ? addHours(from, 7)
+                      : addHours(from, 1), //primeiro turno inicia as 1h. 7h caso seja se veiculos
+                  lt:
+                    input.turn.category === "veiculo"
+                      ? addHours(to, 7)
+                      : addHours(to, 1), //ultimo turno encerra as 1h. 7h caso seja se veiculos
+                },
+              },
             },
           },
           LigacaoTPID: {
@@ -62,7 +70,12 @@ export const vehicleRouter = createTRPCRouter({
   getReport: protectedProcedure
     .input(z.object({ dateRange: dateRangeSchema, turn: turnoSchema }))
     .query(async ({ input }) => {
-      const filter = getTurnFilterQuery(input.dateRange, input.turn);
+      const filter = getTurnFilterQuery(
+        "OM.EnvioEquipeDT",
+        input.dateRange,
+        input.turn,
+      );
+
       const veiculos = await db.$queryRaw<RelatorioVeiculo[]>`
         SELECT
             V.VeiculoID AS id,
@@ -85,8 +98,6 @@ export const vehicleRouter = createTRPCRouter({
             Veiculos V
         LEFT JOIN
             OcorrenciaMovimentacao OM ON V.VeiculoID = OM.VeiculoID
-        LEFT JOIN
-            Ocorrencia O ON O.OcorrenciaID = OM.OcorrenciaID
         WHERE ${filter}
         GROUP BY
             V.VeiculoID, V.VeiculoDS
--- src/utils/getTurnQuery.ts
+++ src/utils/getTurnQuery.ts
@@ -3,7 +3,12 @@ import { DateRange } from "@/hooks/useGlobalDateFilterStore";
 import { Prisma } from "@prisma/client";
 import { differenceInDays, format } from "date-fns";
 
-// !!!!!!!!!! NÃO MECHA A NAO SER QUE SAIBA O QUE ESTA FAZENDO
+// !!!!!!!!!! NÃO MEXA A NAO SER QUE SAIBA O QUE ESTA FAZENDO
+
+// Constantes
+const DATE_FORMAT = "yyyy-MM-dd";
+const TIME_FORMAT = ":00:00";
+type SourceTable = "O.DtHr" | "OM.EnvioEquipeDT";
 
 /**
  * Obtem a query de filtro para os horarios dos turnos nos intervalos de dias selecionados
@@ -11,36 +16,68 @@ import { differenceInDays, format } from "date-fns";
  * @param turn intervalo de turnos
  * @returns
  */
-export const getTurnFilterQuery = (dateRange: DateRange, turn: Turno) => {
+export const getTurnFilterQuery = (
+  source: SourceTable,
+  dateRange: DateRange,
+  turn: Turno,
+) => {
   const selecionouIntevalorDeUmDia =
-    differenceInDays(dateRange.to, dateRange.from) === 1; //true
+    differenceInDays(dateRange.to, dateRange.from) === 1;
+
   if (selecionouIntevalorDeUmDia) {
-    return getTurnFilterUmdia(dateRange, turn);
+    return getTurnFilterUmdia(source, dateRange, turn);
   }
-  return getTurnFilterMaisDeUmDia(dateRange, turn);
+
+  return getTurnFilterMaisDeUmDia(source, dateRange, turn);
 };
 
-export const getTurnFilterMaisDeUmDia = (dateRange: DateRange, turn: Turno) => {
-  const turnoComViradaDeDia = turn.numericFrom >= turn.numericTo;
-  const from = format(dateRange.from, `yyyy-MM-dd ${turn.from}`);
+/** Obtem o Where da query quando o usuario seleciona um periodo de mais de um dia */
+const getTurnFilterMaisDeUmDia = (
+  source: SourceTable,
+  dateRange: DateRange,
+  turn: Turno,
+) => {
+  const turnoComViradaDeDia = turn.from >= turn.to;
+
+  //datas inicial e final
+  const from = format(dateRange.from, `${DATE_FORMAT} ${turn.from}:00:00`);
   const to = turnoComViradaDeDia
-    ? format(dateRange.to, `yyyy-MM-dd ${turn.to}`)
-    : format(dateRange.to, `yyyy-MM-dd 00:00:00`);
+    ? format(dateRange.to, `${DATE_FORMAT} ${turn.to}:00:00`)
+    : format(dateRange.to, `${DATE_FORMAT} 00:00:00`);
+
+  //horario inicial e final
+  const turnFrom = `${turn.from}:00:00`;
+  const turnTo = `${turn.to}:00:00`;
+
+  const _source =
+    source === "O.DtHr" ? Prisma.sql`O.DtHr` : Prisma.sql`OM.EnvioEquipeDT`;
   const operador = turnoComViradaDeDia ? Prisma.sql`OR` : Prisma.sql`AND`;
+
   return Prisma.sql`
-            O.DtHr BETWEEN ${from} AND ${to} 
-            AND (
-              CAST(O.DtHr AS TIME) >= ${turn.from}
-              ${operador} CAST(O.DtHr AS TIME) < ${turn.to}
-            )
-          `;
+    ${_source} BETWEEN ${from} AND ${to} 
+    AND (
+      CAST(${_source} AS TIME) >= ${turnFrom}
+      ${operador} CAST(${_source} AS TIME) < ${turnTo}
+    )
+  `;
 };
 
-export const getTurnFilterUmdia = (dateRange: DateRange, turn: Turno) => {
-  const turnoComViradaDeDia = turn.numericFrom >= turn.numericTo;
-  const from = format(dateRange.from, `yyyy-MM-dd ${turn.from}`);
+/** Obtem o Where da query quando o usuário seleciona um período de um dia */
+const getTurnFilterUmdia = (
+  source: SourceTable,
+  dateRange: DateRange,
+  turn: Turno,
+) => {
+  const turnoComViradaDeDia = turn.from >= turn.to;
+
+  const from = format(dateRange.from, `${DATE_FORMAT} ${turn.from}:00:00`);
+
   const to = turnoComViradaDeDia
-    ? format(dateRange.to, `yyyy-MM-dd ${turn.to}`)
-    : format(dateRange.from, `yyyy-MM-dd ${turn.to}`);
-  return Prisma.sql`O.DtHr BETWEEN ${from} AND ${to}`;
+    ? format(dateRange.to, `${DATE_FORMAT} ${turn.to}:00:00`)
+    : format(dateRange.from, `${DATE_FORMAT} ${turn.to}:00:00`);
+
+  const _source =
+    source === "O.DtHr" ? Prisma.sql`O.DtHr` : Prisma.sql`OM.EnvioEquipeDT`;
+
+  return Prisma.sql`${_source} BETWEEN ${from} AND ${to}`;
 };
